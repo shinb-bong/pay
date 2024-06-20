d3f1f02f7c16240cc5c36ffcc8d6fcee3f4dcf55
coodori <daoh98@naver.com>
2024-06-20 15:10:48 +0900

    Feat: Add QueryDsl

---
+++ src/main/java/summer/pay/config/QueryDslConfig.java
@@ -0,0 +1,20 @@
+package summer.pay.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+
+@Configuration
+public class QueryDslConfig {
+
+	@Autowired
+	private EntityManager em;
+	@Bean
+	public JPAQueryFactory jpaQueryFactory(){
+		return new JPAQueryFactory(em);
+	}
+}
--- src/test/java/summer/pay/PayApplicationTests.java
+++ src/test/java/summer/pay/PayApplicationTests.java
@@ -1,6 +1,5 @@
 package summer.pay;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
