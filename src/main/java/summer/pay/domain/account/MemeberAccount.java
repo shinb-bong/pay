71f58558bd0311da4e6384d8e11665f624667615
coodori <daoh98@naver.com>
2024-06-20 16:16:56 +0900

    Feat: Create MemberAccount

--- src/main/java/summer/pay/domain/account/MemeberAccount.java
+++ src/main/java/summer/pay/domain/account/MemeberAccount.java
@@ -6,25 +6,35 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import summer.pay.config.util.GenerateNumber;
 import summer.pay.domain.Member;
 import summer.pay.domain.type.AccountType;
+import summer.pay.domain.type.BankType;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
 @DiscriminatorValue(AccountType.MEMBER)
 public class MemeberAccount extends Account{
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
 
-	// public MemeberAccount(int deposit, String number, BankType bankType) {
-	// 	super(deposit, number, bankType);
-	// }
+	@Builder
+	public MemeberAccount(int deposit, String number, BankType bankType, Member member) {
+		super(deposit, number, bankType);
+		this.member = member;
+	}
+	public static MemeberAccount createMemberAccount(Member member, BankType bankType){
+		return MemeberAccount.builder()
+			.bankType(bankType)
+			.number(GenerateNumber.generateNumber(bankType))
+			.deposit(0)
+			.member(member)
+			.build();
+	}
+
 }
